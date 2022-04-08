9177ba0f4f2525d17cec02497b39567ff19cb694
thorxx19 <froidefond.olivier@outlook.fr>
2022-04-08 14:21:42 +0200

    MAJ

--- Project02Eclipse/src/com/hemebiotech/analytics/write/CountWriteSymptomDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/write/CountWriteSymptomDataFromFile.java
@@ -17,33 +17,23 @@ public class CountWriteSymptomDataFromFile {
     public void CountWriteSymptom(ArrayList<String> countSymptom){
 
         try {
-            StringBuilder sympton = new StringBuilder();
-            ArrayList<String> newList = new ArrayList<>();
-            Map<String,Integer> map = new HashMap<>();
+            StringBuilder line = new StringBuilder();
+            TreeMap<String,Integer> map = new TreeMap<>();
 
             //search for identical occurrences and their number to write it only once in the file result.out
 
 
-                for (String sym: countSymptom) {
-                    map.putIfAbsent(sym,0);
-                    map.put(sym,map.get(sym)+1);
+                for (String symptom: countSymptom) {
+                    map.putIfAbsent(symptom,0);
+                    map.put(symptom,map.get(symptom)+1);
                 }
                 for (Map.Entry<String,Integer> entry: map.entrySet()) {
-                    newList.add(entry.getKey()+"="+entry.getValue());
+                    line.append(entry.getKey()).append("=").append(entry.getValue()).append("\n");
                 }
-            String[] tabSymptoms1 = newList.toArray(new String[0]);
-
-            // sort occurences alphabetically
-            Arrays.sort(tabSymptoms1);
-
-            for (Object symFin1:tabSymptoms1) {
-                sympton.append(symFin1).append("\n");
-            }
-
 
             // writes symptoms and count occurences in new result.out file
             FileWriter writer = new FileWriter("Project02Eclipse/src/com/hemebiotech/analytics/result/result.out");
-            writer.write(String.valueOf(sympton));
+            writer.write(String.valueOf(line));
             writer.close();
 
         }catch (Exception e){

b00a7755248b1e6792b3414afe3027c6ddd6824d
thorxx19 <froidefond.olivier@outlook.fr>
2022-04-29 13:45:14 +0200

    refractor

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,34 +1,40 @@
-	package com.hemebiotech.analytics;
-	import com.hemebiotech.analytics.analyse.AnalyseSymptomsDataFromFile;
-	import java.util.ArrayList;
-	import java.util.TreeMap;
-	/**
-	 * class principal
-	 * @author o.froidefond
-	 */
-	public class AnalyticsCounter {
-	/**
-	 * construct basic
-	 */
-	public AnalyticsCounter() {
-	}
-	/**
-	 * fonction main
-	 * @param args not used
-	 */
-	public static void main(String[] args){
-		try {
-			AnalyseSymptomsDataFromFile analyseSymptomsDataFromFile = new AnalyseSymptomsDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/resources/symptoms.txt");
-			//read symptoms.txt
-			ArrayList<String> listSymptomsRaw = (ArrayList<String>) analyseSymptomsDataFromFile.getSymptoms();
-			//sort sympoms in ArrayList listSymptoms
-			ArrayList<String> listSymptomsSort = analyseSymptomsDataFromFile.sortSymptoms(listSymptomsRaw);
-			//count symptom
-			TreeMap<String,Integer> mapSymptomCount = analyseSymptomsDataFromFile.countSymptom(listSymptomsSort);
-			//write symptom
-			analyseSymptomsDataFromFile.writeSymptom(mapSymptomCount);
-			}catch(Exception e){
-				System.out.println(e.getMessage());
-		}
-	}
+package com.hemebiotech.analytics;
+
+import com.hemebiotech.analytics.analyse.AnalyseSymptomsDataFromFile;
+
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+/**
+ * class principal
+ *
+ * @author o.froidefond
+ */
+public class AnalyticsCounter {
+    /**
+     * construct basic
+     */
+    public AnalyticsCounter() {
+    }
+
+    /**
+     * fonction main
+     *
+     * @param args not used
+     */
+    public static void main(String[] args) {
+        try {
+            AnalyseSymptomsDataFromFile analyseSymptomsDataFromFile = new AnalyseSymptomsDataFromFile("Project02Eclipse/src/com/hemebiotech/analytics/resources/symptoms.txt");
+            //read symptoms.txt
+            ArrayList<String> listSymptomsRaw = (ArrayList<String>) analyseSymptomsDataFromFile.getSymptoms();
+            //sort sympoms in ArrayList listSymptoms
+            ArrayList<String> listSymptomsSort = analyseSymptomsDataFromFile.sortSymptoms(listSymptomsRaw);
+            //count symptom
+            TreeMap<String, Integer> mapSymptomCount = analyseSymptomsDataFromFile.countSymptom(listSymptomsSort);
+            //write symptom
+            analyseSymptomsDataFromFile.writeSymptom(mapSymptomCount);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- Project02Eclipse/src/com/hemebiotech/analytics/analyse/AnalyseSymptomsDataFromFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/analyse/AnalyseSymptomsDataFromFile.java
@@ -1,120 +1,123 @@
-	package com.hemebiotech.analytics.analyse;
+package com.hemebiotech.analytics.analyse;
 
-	import java.io.File;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.util.*;
 
-	/**
-	 *class for read, sort, count and write different symptoms
-	 *
-	 * @author o.froidefond
-	 * class which parses the file enter @param line by line
-	 * Simple brute force implementation
-	 */
+/**
+ * class for read, sort, count and write different symptoms
+ *
+ * @author o.froidefond
+ * class which parses the file enter @param line by line
+ * Simple brute force implementation
+ */
 
-	public class AnalyseSymptomsDataFromFile implements ISymptomReader {
-		private final String filepath;
+public class AnalyseSymptomsDataFromFile implements ISymptomReader {
+    private final String filepath;
 
-		/**
-		 * construct
-		 *
-		* @param filepath a full or partial path to file with symptom strings in it, one per line
-		*/
-		public AnalyseSymptomsDataFromFile(String filepath) {
-			this.filepath = filepath;
-		}
+    /**
+     * construct
+     *
+     * @param filepath a full or partial path to file with symptom strings in it, one per line
+     */
+    public AnalyseSymptomsDataFromFile(String filepath) {
+        this.filepath = filepath;
+    }
 
-	/**
-	 * method (getSymptoms) for read symptom in file
-	 *
-	 * @return string array of symptoms read from the file symptoms
-	 */
-	@Override
-	public List<String> getSymptoms() {
+    /**
+     * method (getSymptoms) for read symptom in file
+     *
+     * @return string array of symptoms read from the file symptoms
+     */
+    @Override
+    public List<String> getSymptoms() {
 
-		ArrayList<String> result = new ArrayList<String>();
-		//line-by-line reading of the file and integration in the array result
-		if (filepath != null) {
-			try {
+        ArrayList<String> result = new ArrayList<String>();
+        //line-by-line reading of the file and integration in the array result
+        if (filepath != null) {
+            try {
 
-				File file = new File(filepath);
-				Scanner scanner = new Scanner(file);
+                File file = new File(filepath);
+                Scanner scanner = new Scanner(file);
 
-				while (scanner.hasNextLine()){
-					result.add(scanner.nextLine());
-				}
-			} catch (FileNotFoundException e) {
-				System.out.println("file not found Exception");
-			}
-		}
-		return result;
-	}
+                while (scanner.hasNextLine()) {
+                    result.add(scanner.nextLine());
+                }
+            } catch (FileNotFoundException e) {
+                System.out.println("file not found Exception");
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * method (sortSymptoms) sort the symptoms in alphabetical order
-	 *
-	 * @param symptoms is raw array list of symptoms
-	 * @return listSymptomsSort is array sort of symptoms
-	 *
-	 */
-	public ArrayList<String> sortSymptoms(ArrayList<String> symptoms) {
-		ArrayList<String> listSymptomsSort = new ArrayList<>();
+    /**
+     * method (sortSymptoms) sort the symptoms in alphabetical order
+     *
+     * @param symptoms is raw array list of symptoms
+     * @return listSymptomsSort is array sort of symptoms
+     */
+    public ArrayList<String> sortSymptoms(ArrayList<String> symptoms) {
+        ArrayList<String> listSymptomsSort = new ArrayList<>();
 
-		try {
+        try {
 
-			symptoms.sort(String::compareTo);
+            symptoms.sort(String::compareTo);
 
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return symptoms;
-	}
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return symptoms;
+    }
 
-	/**
-	 *method (countSymptoms) countSymptom for count different symptoms
-	 *
-	 * @param symptoms array with the symptoms retrieved in a .txt file
-	 * @return map is TreeMap count of different symptoms
-	 */
-	public TreeMap<String, Integer> countSymptom(ArrayList<String> symptoms){
-		 final TreeMap<String,Integer> map = new TreeMap<>();
-		try {
-			//search for identical occurrences and their number to write it only once in the file result.out
+    /**
+     * method (countSymptoms) countSymptom for count different symptoms
+     *
+     * @param symptoms array with the symptoms retrieved in a .txt file
+     * @return map is TreeMap count of different symptoms
+     */
+    public TreeMap<String, Integer> countSymptom(ArrayList<String> symptoms) {
+        final TreeMap<String, Integer> map = new TreeMap<>();
+        try {
+            //search for identical occurrences and their number to write it only once in the file result.out
 
-			for (String symptom: symptoms) {
-				map.putIfAbsent(symptom,0);
-				map.put(symptom,map.get(symptom)+1);
-			}
+            for (String symptom : symptoms) {
+                map.putIfAbsent(symptom, 0);
+                map.put(symptom, map.get(symptom) + 1);
+            }
 
-		}catch (Exception e){
-			System.out.println(e.getMessage());
-		}
-		return map;
-	}
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return map;
+    }
 
-	/**
-	 * method (writeSymptoms) write symptoms in file result.out
-	 *
-	 * @param map is array symptom sort and count
-	 * fonction write the new file result.out
-	 */
-	public void writeSymptom(Map<String,Integer> map){
-		final StringBuilder line = new StringBuilder();
+    /**
+     * method (writeSymptoms) write symptoms in file result.out
+     *
+     * @param map is array symptom sort and count
+     *            fonction write the new file result.out
+     * @return filresult
+     */
+    public File writeSymptom(Map<String, Integer> map) {
+        final StringBuilder line = new StringBuilder();
+        File fileResult = new File("Project02Eclipse/src/com/hemebiotech/analytics/result/result.out");
+        try {
 
-		try {
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                line.append(entry.getKey()).append("=").append(entry.getValue()).append("\n");
+            }
+            // writes symptoms in new result.out file
+            FileWriter writer = new FileWriter(fileResult);
+            writer.write(String.valueOf(line));
+            writer.close();
 
-			for (Map.Entry<String,Integer> entry: map.entrySet()) {
-				line.append(entry.getKey()).append("=").append(entry.getValue()).append("\n");
-			}
-			// writes symptoms in new result.out file
-			FileWriter writer = new FileWriter("Project02Eclipse/src/com/hemebiotech/analytics/result/result.out");
-			writer.write(String.valueOf(line));
-			writer.close();
+            return fileResult;
 
-		}catch (Exception e){
-			System.out.println(e.getMessage());
-		}
-	}
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 
 }
